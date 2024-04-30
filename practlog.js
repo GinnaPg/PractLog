3c12dca1fe36e1d0a504984605c51aafc2acb653
Ginna Pg <itzandypg@gmail.com>
2024-04-30 17:50:47 -0600

    Agregado

---
+++ practlog.js
@@ -0,0 +1,20 @@
+//USER PROFILE
+
+
+//HIGHEST NUMBER
+
+let userNumber = [];
+ userNumber = prompt ("Escribe 10 numeros, separalos usando comas.")
+
+ let numeros = userNumber.split(",").map(mumero);
+ let max = numeros[0];
+
+ for (let i = 1; i < numeros.length; i++);
+ if (numeros[i] > max){
+    max = numeros[i];
+ }
+console.log("El numero mas grande es:" + max);
+//ALARM
+//PALINDRONE
+//FACTORIAL
+//FLAT ARRAY
